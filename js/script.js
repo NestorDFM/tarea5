06d83f124a5dd04c651a794bd02d5cf2e676d46a
EduardoEspinola <eduardore44684@gmail.com>
2016-09-01 21:32:00 -0400

    tarea5

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+//funcion anonima autoejecutable
+(function(){
+	console.log('Hola Mundo');
+})();
+
+//variables
+var descripcion = '';
+var cantidad = 0;
+
+//array
+var array = [];
+array.push('Hola Mundo');
+array.push('JavaScript');
+console.log(array);
+
+//object
+var objeto = {}
+
+objeto.cantidad=10;
+objeto.descripcion='Descripcion';
+console.log(objeto);
